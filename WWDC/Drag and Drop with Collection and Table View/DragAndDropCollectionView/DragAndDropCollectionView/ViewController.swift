914b4db63f4486841d057e45e481e73e30dac09d
Joons <elddy0948@gmail.com>
2021-04-11 22:34:45 +0900

    Feat : DragDelegate, DropDelegate 구현

--- WWDC/Drag and Drop with Collection and Table View/DragAndDropCollectionView/DragAndDropCollectionView/ViewController.swift
+++ WWDC/Drag and Drop with Collection and Table View/DragAndDropCollectionView/DragAndDropCollectionView/ViewController.swift
@@ -4,7 +4,7 @@ class ViewController: UIViewController {
     enum Section {
         case main
     }
-
+    
     var collectionView: UICollectionView!
     var diffableDataSource: UICollectionViewDiffableDataSource<Section, String>!
     var dataToPresent: [String] = ["Hello World!", "Swift is Good!", "iOS Lover!"]
@@ -22,6 +22,9 @@ extension ViewController {
     private func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: configureCollectionViewLayout())
         view.addSubview(collectionView)
+        collectionView.dragDelegate = self
+        collectionView.dropDelegate = self
+        collectionView.dragInteractionEnabled = true
         collectionView.backgroundColor = .secondarySystemBackground
         collectionView.register(MyCell.self, forCellWithReuseIdentifier: MyCell.reuseIdentifier)
     }
@@ -58,5 +61,75 @@ extension ViewController {
         snapshot.appendItems(dataToPresent)
         
         diffableDataSource.apply(snapshot)
+    }
+    
+    private func reorderDataSource(string: String, source: IndexPath, destination: IndexPath) {
+        var snapshot = diffableDataSource.snapshot()
+        
+        guard let lastItem = diffableDataSource.itemIdentifier(for: destination) else {
+            return
+        }
+        
+        if source.row > destination.row {
+            snapshot.moveItem(string, beforeItem: lastItem)
+        } else {
+            snapshot.moveItem(string, afterItem: lastItem)
+        }
+        
+        diffableDataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension ViewController: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        guard let item = diffableDataSource.itemIdentifier(for: indexPath) else {
+            return []
+        }
+        let itemProvider = NSItemProvider(object: item as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        return [dragItem]
+    }
+}
+
+extension ViewController: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        guard session.items.count == 1 else {
+            return UICollectionViewDropProposal(operation: .cancel)
+        }
+        
+        if collectionView.hasActiveDrag {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        } else {
+            return UICollectionViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        guard let dragItem = coordinator.items.first?.dragItem,
+              let sourceIndexPath = coordinator.items.first?.sourceIndexPath else {
+            return
+        }
+        let destinationIndexPath: IndexPath
+        
+        if let indexPath = coordinator.destinationIndexPath {
+            destinationIndexPath = indexPath
+        } else {
+            let section = collectionView.numberOfSections - 1
+            let row = collectionView.numberOfItems(inSection: section)
+            destinationIndexPath = IndexPath(row: row, section: section)
+        }
+        
+        dragItem.itemProvider.loadObject(ofClass: String.self, completionHandler: { [weak self] (string, error) in
+            guard let self = self,
+                  error == nil,
+                  let string = string else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.reorderDataSource(string: string, source: sourceIndexPath, destination: destinationIndexPath)
+            }
+        })
+        coordinator.drop(dragItem, toItemAt: destinationIndexPath)
+        print(destinationIndexPath)
     }
 }
